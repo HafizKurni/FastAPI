b52807aefe35fbd5c021eecb5f18a090267ccc17
HafizKurni <kurni.hafiz2002@gmail.com>
2022-06-11 14:46:53 +0700

    updated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from random import Random
+import re
+from textwrap import indent
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -25,6 +27,12 @@ def find_post(id):
             return p
 
 
+def find_index_post(id):
+    for i, p in enumerate(my_post):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -35,7 +43,7 @@ def get_posts():
     return {"data": my_post}
 
 
-@app.post("/post")
+@app.post("/post", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000)
@@ -44,8 +52,37 @@ def create_posts(post: Post):
 
 
 @app.get("/post/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
 
     post = find_post(id)
-    print(post)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
     return{"post_detail": post}
+
+
+@app.delete("/post/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not exist")
+
+    my_post.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/post/{id}")
+def update_post(id: int, post: Post):
+
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not exist")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_post[index] = post_dict
+    return {'data': post_dict}
